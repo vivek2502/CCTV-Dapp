90f13f4c309883813055fc2d578abe723b5cfd9e
vivek2502 <vivekgupta.mailme@gmail.com>
2022-06-14 23:10:26 +0530

    Updates

---
+++ precache-manifest.468602d50678c4e4206fbaa52db1434f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92cdf1c060b4824ce2df93ee946e3a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af21b6ae2017fca045bb",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "593ea68fa5a0428381ef",
+    "url": "./static/css/main.1511418a.chunk.css"
+  },
+  {
+    "revision": "af21b6ae2017fca045bb",
+    "url": "./static/js/2.68fd61cc.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "./static/js/2.68fd61cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593ea68fa5a0428381ef",
+    "url": "./static/js/main.ceb94b55.chunk.js"
+  },
+  {
+    "revision": "fc71048510e0974eab3b",
+    "url": "./static/js/runtime-main.be13530a.js"
+  }
+]);
