7c4f94d4f9f2f1bd39932eca4d7e659d31b164be
Vivek Kumar/LGSIA Model Team(vivek.kumar@lge.com) <vivek.kumar@lge.com>
2022-07-19 15:35:54 +0530

    Updates

---
+++ precache-manifest.9d0cd815c7a11d424559abcf3f2955ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e35647f2ff8cc45714516792bbe64f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "206600610961946a1b1a",
+    "url": "./static/css/2.9939c636.chunk.css"
+  },
+  {
+    "revision": "6dae5b2d33684c9bddf1",
+    "url": "./static/css/main.a1469a32.chunk.css"
+  },
+  {
+    "revision": "206600610961946a1b1a",
+    "url": "./static/js/2.9e418afa.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "./static/js/2.9e418afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dae5b2d33684c9bddf1",
+    "url": "./static/js/main.cbf49fb7.chunk.js"
+  },
+  {
+    "revision": "fc71048510e0974eab3b",
+    "url": "./static/js/runtime-main.be13530a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4b403569e756e262c76d4371c5479dd8.js"
+  "./precache-manifest.9d0cd815c7a11d424559abcf3f2955ab.js"
 );
 
 self.addEventListener('message', (event) => {
